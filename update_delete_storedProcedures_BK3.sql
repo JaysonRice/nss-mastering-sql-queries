86bad5a9c112a43e3389e6e10b17076908f04dda
Jayson Rice <jayson.rice.95@gmail.com>
2020-09-15 15:35:07 -0400

    Updated stored procedures and triggers.

--- update_delete_storedProcedures_BK3.sql
+++ update_delete_storedProcedures_BK3.sql
@@ -1,5 +1,27 @@
 -- Book 3, CH 1: Updates
 
+create table OilChangeLogs (
+  oil_change_log_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  date_occured timestamp with time zone,
+  vehicle_id int,
+  FOREIGN KEY (vehicle_id) REFERENCES Vehicles (vehicle_id)
+);
+
+create table RepairTypes (
+  repair_type_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(50)
+);
+
+create table CarRepairTypeLogs (
+  car_repair_type_log_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  date_occured timestamp with time zone,
+  vehicle_id int,
+  repair_type_id INT,
+  FOREIGN KEY (vehicle_id) REFERENCES Vehicles (vehicle_id),
+  FOREIGN KEY (repair_type_id) REFERENCES RepairTypes (repair_type_id)
+);
+
+
 -- Rheta Raymen an employee of Carnival has asked to be transferred to a different dealership location.
 -- She is currently at dealership 751.
 -- She would like to work at dealership 20. Update her record to reflect her transfer.
@@ -96,4 +118,56 @@ SELECT * FROM vehicles
 WHERE is_sold = true
 
 
+-- Carnival would also like to handle the case for when a car gets returned by a customer. 
+-- When this occurs they must add the car back to the inventory and mark the original sales record as returned = True.
+
+CREATE OR REPLACE PROCEDURE return_sold_vehicle(in vehicleId int)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	UPDATE sales
+	SET returned = true
+	WHERE vehicle_id = vehicleId;
+		
+	UPDATE vehicles
+	SET is_sold = false
+	WHERE vehicle_id = vehicleId;
+	
+END
+$$;
+
+SELECT v.vehicle_id, v.vin, s.returned, v.is_sold FROM sales s
+JOIN vehicles v ON v.vehicle_id = s.vehicle_id
+WHERE v.vehicle_id = 1;
+
+CALL return_sold_vehicle(1);
+
+-- Carnival staff are required to do an oil change on the returned car before putting it back on the sales floor. 
+-- In our stored procedure, we must also log the oil change within the OilChangeLog table.
+-- Located at the top of the file
+
+CREATE OR REPLACE PROCEDURE return_sold_vehicle_with_oil_change(in vehicleId int)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	UPDATE sales
+	SET returned = true
+	WHERE vehicle_id = vehicleId;
+		
+	UPDATE vehicles
+	SET is_sold = false
+	WHERE vehicle_id = vehicleId;
+	
+	INSERT INTO oilchangelogs(date_occured, vehicle_id)
+	VALUES (CURRENT_DATE, vehicleId);
+	
+END
+$$;
+
+SELECT v.vehicle_id, v.vin, s.returned, v.is_sold, o.date_occured FROM vehicles v
+LEFT JOIN sales s ON v.vehicle_id = s.vehicle_id
+LEFT JOIN oilchangelogs o ON v.vehicle_id = o.vehicle_id
+WHERE v.vehicle_id = 2;
+
+CALL return_sold_vehicle_with_oil_change(2);
 
