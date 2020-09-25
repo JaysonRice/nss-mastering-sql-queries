96a9c8b1d34f3d4b03601f6b7ea5fb6c5fa508ac
Jayson Rice <jayson.rice.95@gmail.com>
2020-09-25 15:27:23 -0400

    Added views for reports

--- optimizations_project_BK4.sql
+++ optimizations_project_BK4.sql
@@ -3,7 +3,6 @@
 -- Indexes
 
 -- Often need to see sales data from particular dealerships, employees, or customers
--- Index all 3
 CREATE INDEX sales_employee_idx ON sales
 (
     employee_id
@@ -12,6 +11,7 @@ CREATE INDEX sales_employee_idx ON sales
 EXPLAIN ANALYZE SELECT * FROM sales
 WHERE employee_id = 2;
 
+-- Find sales by customer_id
 CREATE INDEX sales_customer_idx ON sales
 (
     customer_id
@@ -85,10 +85,10 @@ END
 $$;
 
 -- Views
-
+-- Track employee sales metrics
 CREATE OR REPLACE VIEW employee_sales_metrics AS
   SELECT 
-    CONCAT (e.first_name, '', e.last_name) AS full_name,
+    CONCAT (e.first_name, ' ', e.last_name) AS full_name,
     SUM (s.price) AS sum_of_sales,
 	ROUND (AVG (s.price),2) AS average_sale_price,
 	COUNT (s.sale_id) AS number_of_sales
@@ -98,3 +98,35 @@ CREATE OR REPLACE VIEW employee_sales_metrics AS
   ORDER BY sum_of_sales DESC;
 
 SELECT * from employee_sales_metrics
+
+-- Track dealership sales metrics
+CREATE OR REPLACE VIEW dealership_sales_metrics AS
+  SELECT 
+    d.business_name,
+    SUM (s.price) AS sum_of_sales,
+	ROUND (AVG (s.price),2) AS average_sale_price,
+	COUNT (s.sale_id) AS number_of_sales
+	FROM dealerships d
+  INNER JOIN sales s ON s.dealership_id = d.dealership_id
+  GROUP BY d.business_name
+  ORDER BY number_of_sales DESC;
+
+SELECT * from dealership_sales_metrics
+
+-- Track most popular vehicles
+CREATE OR REPLACE VIEW vehicle_type_sold_metrics AS
+  SELECT
+  CONCAT (vmk.name , ' ', vmd.name , ' ', vbt.name) AS vehicle_name, COUNT(vt.vehicle_type_id) AS number_sold
+	FROM vehicles v
+  INNER JOIN sales s ON s.vehicle_id = v.vehicle_id
+  INNER JOIN VehicleTypes vt ON vt.vehicle_type_id = v.vehicle_type_id
+  INNER JOIN vehiclemakes vmk ON vt.make_id = vmk.vehicle_make_id
+  INNER JOIN vehiclemodels vmd ON vt.model_id = vmd.vehicle_model_id
+  INNER JOIN vehiclebodytypes vbt ON vt.body_type_id = vbt.vehicle_body_type_id
+  GROUP BY vt.vehicle_type_id, vmk.name, vmd.name, vbt.name
+  ORDER BY number_sold DESC;
+
+EXPLAIN ANALYZE SELECT * from vehicle_type_sold_metrics
+
+
+
